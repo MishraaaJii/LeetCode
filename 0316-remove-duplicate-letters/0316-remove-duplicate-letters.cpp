f0b93983ca1bf702eb70d8add13921236af0810f
Cinnamon <arpit411mishra@gmail.com>
2026-07-19 12:53:17 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.99%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> mpp(26, 0);
+        stack<char> st;
+        vector<bool> seen(26, false);
+        string ans = "";
+        for(int i = 0; i < s.size(); i++){
+            mpp[s[i] - 'a'] += 1;
+        }
+        char c;
+        for(int i = 0; i < s.size(); i++){
+            c = s[i];
+            mpp[c - 'a']--;
+            if(seen[c - 'a']) continue;
+            while(!st.empty() && st.top() >= c && mpp[st.top() - 'a'] > 0){
+                seen[st.top() - 'a'] = false;
+                st.pop();
+            }
+            st.push(c);
+            seen[c - 'a'] = true;
+        }
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        for(int i = 0; i < ans.size()/2; i++){
+            c = ans[i];
+            ans[i] = ans[ans.size() - 1 - i];
+            ans[ans.size() - 1 - i] = c;
+        }
+        return ans;
+    }
+};
