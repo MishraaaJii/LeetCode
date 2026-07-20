f97e923898a45a80cdccf4fb720025255929540d
Cinnamon <arpit411mishra@gmail.com>
2026-07-20 20:19:29 +0530

    Time: 0 ms (100%), Space: 12.9 MB (6.23%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -3,9 +3,8 @@ public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         sort(arr.begin(), arr.end());
         if(arr.size() == 1) return true;
-        int diff = arr[1] - arr[0];
         for(int i = 2; i < arr.size(); i++){
-            if(arr[i] - arr[i-1] != diff) return false;
+            if((arr[i] - arr[i-1]) != (arr[i-1] - arr[i-2])) return false;
         }
         return true;
     }
