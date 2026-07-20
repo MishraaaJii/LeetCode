abb8930404698a6bb01f80c79934af33871cb21e
Cinnamon <arpit411mishra@gmail.com>
2026-07-20 18:37:13 +0530

    Time: 2 ms (44.68%), Space: 16.3 MB (9.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -7,24 +7,28 @@ public:
         comb(candidates, target, 0, ans, curr);
         return ans;
     }
-    void comb(vector<int>& candidates, int target, int index, vector<vector<int>>& ans, vector<int>& curr){
-        if(!target){
+    void comb(vector<int>& candidates, int target, int index,
+              vector<vector<int>>& ans, vector<int>& curr) {
+        if (!target) {
             ans.push_back(curr);
             return;
         }
-        if(index == candidates.size()) return;
+        if (index == candidates.size())
+            return;
+        if (target < candidates[index])
+            return;
 
-        //notTake
+        // notTake
         int newIndex = index;
-        while(newIndex < candidates.size() && candidates[index] == candidates[newIndex]) newIndex++;
+        while (newIndex < candidates.size() &&
+               candidates[index] == candidates[newIndex])
+            newIndex++;
         comb(candidates, target, newIndex, ans, curr);
-        //take
-        if(target >= candidates[index]) {
-            curr.push_back(candidates[index]);
-            target = target - candidates[index];
-            comb(candidates, target, index+1, ans, curr);
-            curr.pop_back();
-        }
+        // take
+        curr.push_back(candidates[index]);
+        target = target - candidates[index];
+        comb(candidates, target, index + 1, ans, curr);
+        curr.pop_back();
         return;
     }
 };
