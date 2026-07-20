c5aaafcae90b9c99baf16c98134703adc674a61d
Cinnamon <arpit411mishra@gmail.com>
2026-07-20 18:35:04 +0530

    Time: 3 ms (38%), Space: 16.3 MB (9.54%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        comb(candidates, target, 0, ans, curr);
+        return ans;
+    }
+    void comb(vector<int>& candidates, int target, int index, vector<vector<int>>& ans, vector<int>& curr){
+        if(!target){
+            ans.push_back(curr);
+            return;
+        }
+        if(index == candidates.size()) return;
+
+        //notTake
+        int newIndex = index;
+        while(newIndex < candidates.size() && candidates[index] == candidates[newIndex]) newIndex++;
+        comb(candidates, target, newIndex, ans, curr);
+        //take
+        if(target >= candidates[index]) {
+            curr.push_back(candidates[index]);
+            target = target - candidates[index];
+            comb(candidates, target, index+1, ans, curr);
+            curr.pop_back();
+        }
+        return;
+    }
+};
