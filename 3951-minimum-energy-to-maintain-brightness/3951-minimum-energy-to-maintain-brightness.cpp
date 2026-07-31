fd0537dd7d73f350d12d6f9399b99bc4af66935a
Cinnamon <arpit411mishra@gmail.com>
2026-07-31 11:39:34 +0530

    Time: 80 ms (58.5%), Space: 203.6 MB (73.71%) - LeetHub

--- 3951-minimum-energy-to-maintain-brightness/3951-minimum-energy-to-maintain-brightness.cpp
+++ 3951-minimum-energy-to-maintain-brightness/3951-minimum-energy-to-maintain-brightness.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long minEnergy(int n, int brightness, vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        long long bulbs = ceil(brightness/3.0);
+        long long bulbs = (brightness + 2) / 3;
         long long time = 0;
         int start = intervals[0][0];
         int end = intervals[0][1];
