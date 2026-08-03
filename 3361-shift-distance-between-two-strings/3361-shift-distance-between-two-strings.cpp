8b2ae44d0e153d1eccbfc98121c4074df544ccf7
Cinnamon <arpit411mishra@gmail.com>
2026-08-03 15:09:02 +0530

    Time: 98 ms (37.87%), Space: 62.3 MB (5.33%) - LeetHub

--- 3361-shift-distance-between-two-strings/3361-shift-distance-between-two-strings.cpp
+++ 3361-shift-distance-between-two-strings/3361-shift-distance-between-two-strings.cpp
@@ -2,24 +2,36 @@ class Solution {
 public:
     long long shiftDistance(string s, string t, vector<int>& nextCost, vector<int>& previousCost) {
         long long cost = 0;
+        vector<vector<long long>> costs(27, vector<long long>(27, 0));
+        for(char i = 'a'; i <= 'z'; i++){
+            for(char j = 'a'; j <= 'z'; j++){
+                if(i == j) continue;
+
+                char temp = i;
+                //prev
+                long long prev = 0;
+                while(i != j){
+                    prev += previousCost[i - 'a'];
+                    if(i == 'a') i = 'z';
+                    else i--;
+                }
+                i = temp;
+                //next
+                long long next = 0;
+                while(i != j){
+                    next += nextCost[i - 'a'];
+                    if(i == 'z') i = 'a';
+                    else i++;
+                }
+                i = temp;
+
+                costs[i - 'a'][j - 'a'] = min(prev, next);
+                
+            }
+        }
+
         for(int i = 0; i < s.size(); i++){
-            char temp = s[i];
-            //prev
-            long long prev = 0;
-            while(s[i] != t[i]){
-                prev += previousCost[s[i] - 'a'];
-                if(s[i] == 'a') s[i] = 'z';
-                else s[i]--;
-            }
-            s[i] = temp;
-            //next
-            long long next = 0;
-            while(s[i] != t[i]){
-                next += nextCost[s[i] - 'a'];
-                if(s[i] == 'z') s[i] = 'a';
-                else s[i]++;
-            }
-            cost += min(prev, next);
+            cost += costs[s[i] - 'a'][t[i] - 'a'];
         }
         return cost;
     }
