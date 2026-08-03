b1b9d1b38f53fcf509c327d26ddbe3910e2725fe
Cinnamon <arpit411mishra@gmail.com>
2026-08-03 19:29:44 +0530

    Time: 3 ms (79.87%), Space: 93.9 MB (59.42%) - LeetHub

--- 2826-sorting-three-groups/2826-sorting-three-groups.cpp
+++ 2826-sorting-three-groups/2826-sorting-three-groups.cpp
@@ -8,25 +8,10 @@ public:
             if(nums[i] >= temp){
                 dp.push_back(nums[i]);
             } else {
-                dp[binarySearch(dp, nums[i])] = nums[i];
+                auto it = upper_bound(dp.begin(), dp.end(), nums[i]);
+                *it = nums[i];
             }
         }
         return nums.size() - dp.size();
-    }
-    int binarySearch(vector<int>& dp, int num){
-        int start = 0;
-        int end = dp.size() - 1;
-        int mid;
-        int index = -1;
-        while(start <= end){
-            mid = (start + end)/2;
-            if(dp[mid] > num){
-                index = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return index;
     }
 };
