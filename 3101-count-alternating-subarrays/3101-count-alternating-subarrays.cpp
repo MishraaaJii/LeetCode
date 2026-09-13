5212b19624b9bcf7c149e83480f0d11491da32f8
Cinnamon <arpit411mishra@gmail.com>
2026-09-13 14:04:08 +0530

    Time: 5 ms (21.86%), Space: 118 MB (23.5%) - LeetHub

---
+++ 3101-count-alternating-subarrays/3101-count-alternating-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        long long ans = 0;
+        long long len = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0 || nums[i] != nums[i - 1])
+                len++;
+            else
+                len = 1;
+
+            ans += len;
+        }
+
+        return ans;        
+    }
+};
